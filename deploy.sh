b49703ad5af3c8ae793e4083d03b9402d0cddfcb
Lucas Cairns <lucas.cairns@digital.homeoffice.gov.uk>
2018-06-04 09:16:02 +0100

    Lc implement kd and drone (#2)
    * removed redundant lines from proxy config
    
    * added templating to resources, deploy script and created drone file
    
    * added templating to resources, deploy script and created drone file
    
    * fixed kc url
    
    * fixed typo

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+export KUBE_NAMESPACE=${KUBE_NAMESPACE}
+export KUBE_SERVER=${KUBE_SERVER}
+
+if [[ -z ${VERSION} ]] ; then
+    export VERSION=${IMAGE_VERSION}
+fi
+
+if [[ ${ENVIRONMENT} == "prod" ]] ; then
+    echo "deploy ${VERSION} to PROD namespace, using CS_PROD drone secret"
+    export KUBE_TOKEN=${CS_PROD}
+    export KC_URL=sso.digital.homeoffice.gov.uk
+else
+    if [[ ${ENVIRONMENT} == "qa" ]] ; then
+        echo "deploy ${VERSION} to QA namespace, using CS_QA drone secret"
+        export KUBE_TOKEN=${CS_QA}
+    else
+        echo "deploy ${VERSION} to DEV namespace, using CS_DEV drone secret"
+        export KUBE_TOKEN=${CS_DEV}
+    fi
+    export KC_URL=sso-dev.notprod.homeoffice.gov.uk
+fi
+
+if [[ -z ${KUBE_TOKEN} ]] ; then
+    echo "Failed to find a value for KUBE_TOKEN - exiting"
+    exit -1
+fi
+
+echo
+echo "Deploying Network Policies and Outbound Proxy to ${ENVIRONMENT} @ version: ${VERSION}"
+echo
+
+cd kd
+
+kd --insecure-skip-tls-verify \
+    -f ./kube/hocs-backend-networkpolicy.yaml \
+    -f ./kube/hocs-frontend-networkpolicy.yaml \
+    -f ./kube/hocs-outbound-proxy-networkpolicy.yaml \
+    -f ./kube/hocs-outbound-proxy-configmap.yaml \
+    -f ./kube/hocs-outbound-proxy-service.yaml \
+    -f ./kube/hocs-outbound-proxy-deployment.yaml
