f5034ff9396a7af47b1e67f39114f3778f711fa8
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-08-30 22:50:44 +0100

    test drone yml

--- setup-kube.sh
+++ setup-kube.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env ash
 
-curl https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${KUBE_CLUSTER}.crt \
+wget https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${KUBE_CLUSTER}.crt \
   --output /tmp/cluster_ca.crt
 
 kubectl config set-cluster "${KUBE_CLUSTER}" \
@@ -10,9 +10,9 @@ kubectl config set-cluster "${KUBE_CLUSTER}" \
 kubectl config set-credentials helm \
   --token="${KUBE_TOKEN}"
 
-kubectl config set-context circleci \
+kubectl config set-context helm \
   --cluster="${KUBE_CLUSTER}" \
   --user="helm" \
   --namespace="${KUBE_NAMESPACE}"
 
-kubectl config use-context circleci
+kubectl config use-context helm
