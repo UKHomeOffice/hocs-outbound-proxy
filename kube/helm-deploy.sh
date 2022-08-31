bcce8107fe02a021231ec5a49323573cb29a51b8
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-08-31 20:13:44 +0100

    test drone yml

--- kube/helm-deploy.sh
+++ kube/helm-deploy.sh
@@ -8,6 +8,6 @@ helm upgrade hocs-outbound-proxy \
 --reset-values \
 --timeout 3m \
 --history-max 3 \
---namespace ${DRONE_STEP_NAME} \
+--namespace ${KUBE_NAMESPACE} \
 --set version=${VERSION} \
 ${VALUES_FILE}
